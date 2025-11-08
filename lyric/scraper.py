aceb98269bb9ddb7cab7c8a182a770169156539a
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-08 17:12:16 +0800

    feat: add function to scrape lyrics
    add missing spaces

--- lyric/scraper.py
+++ lyric/scraper.py
@@ -1,9 +1,15 @@
 import json
 import os
+import re
+
 import bs4
 import requests
+from bs4.element import NavigableString, Tag
 from dotenv import load_dotenv
+
 load_dotenv()
+
+
 def fetch_songs(query: str):
     base_url = "https://api.genius.com/search"
     header = {"Authorization": f"Bearer {os.getenv('GENIUS_API_KEY')}"}
@@ -17,7 +23,37 @@ def fetch_songs(query: str):
     return songs
 
 
+def fetch_lyrics(url: str):
+    response = requests.get(url)
+    results = response.text
+    soup = bs4.BeautifulSoup(results, "html.parser")
+    headers = soup.find_all("div", class_=re.compile("LyricsHeader"))
+    if headers:
+        for header in headers:
+            header.decompose()
+
+    lyrics = ""
+    lyric_container = soup.find_all("div", attrs={"data-lyrics-container": "true"})
+    for lyric in lyric_container:
+        if not lyric.contents:
+            lyrics += "\n"
+            continue
+        for content in lyric.contents:
+            print(content, type(content))
+            assert isinstance(content, (NavigableString, Tag))
+            if content.name == "br":
+                lyrics += "\n"
+            elif isinstance(content, NavigableString):
+                lyrics += str(content)
+            elif content.get("data-exclude-from-selection") != "true":
+                lyrics += content.get_text(separator="\n")
+    return lyrics
+
+
 def main():
     songs = fetch_songs("let it")
+    lyrics = fetch_lyrics(songs[7]["url"])
+    print(lyrics)
+
 
 main()
