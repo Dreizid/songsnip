80a02dbc5f2bf481338578fceaeff94179834227
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-14 15:55:26 +0800

    feat: add utility function to convert audio files to wav

---
+++ audio/preprocess.py
@@ -0,0 +1,22 @@
+import atexit
+import os
+import subprocess
+import tempfile
+
+
+def convert_to_wav(input_path: str) -> str:
+    """
+    Convert any audio format into .wav format.
+    """
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete_on_close=False) as tmpfile:
+        output_path = tmpfile.name
+    process = subprocess.run(
+        ["ffmpeg", "-i", input_path, "-y", output_path]
+        check=True,
+        stdout=subprocess.subprocess.DEVNULL,
+        stderr=subprocess.subprocess.DEVNULL,
+        text=True
+    )
+
+    atexit.register(lambda: os.path.exists(output_path) and os.remove(output_path))
+    return output_path
