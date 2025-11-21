c4245a4f08c8123626a0dd49db74805a2b5d6578
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-21 18:05:00 +0800

    feat: add utility to split the vocals from instruments for better alignment

--- audio/splitter.py
+++ audio/splitter.py
@@ -1,10 +1,19 @@
-def split_audio(input_path: str, backend: str = "none"):
-    pass
+import atexit
+import tempfile
+from typing import Optional
+
+import demucs.separate
+
+TMPDIR = tempfile.TemporaryDirectory()
+atexit.register(TMPDIR.cleanup)
 
 
-def _split_demucs(input_path: str):
-    pass
+def split_audio(audio_path: str, output_path: Optional[str] = None) -> str:
+    args = ["--two-stems", "vocals", "-n", "htdemucs_ft", audio_path]
+    if output_path is not None:
+        args.extend(["-o", output_path])
+    else:
+        args.extend(["-o", TMPDIR.name])
+    demucs.separate.main(args)
 
-
-def _split_spleeter(input_path: str):
-    pass
+    return TMPDIR.name if output_path is None else output_path
