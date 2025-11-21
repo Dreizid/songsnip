4fb771deb9fc96fe46010c456fcc60fb8d91fadd
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-21 18:43:47 +0800

    feat: add verbose mode for preprocess in case ffmpeg fails

--- audio/preprocess.py
+++ audio/preprocess.py
@@ -4,7 +4,7 @@ import subprocess
 import tempfile
 
 
-def convert_to_wav(input_path: str) -> str:
+def convert_to_wav(input_path: str, verbose: bool = False) -> str:
     """
     Convert any audio format into .wav format.
     """
@@ -12,11 +12,19 @@ def convert_to_wav(input_path: str) -> str:
         raise FileNotFoundError(f"Input file '{input_path}' does not exist.")
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
         output_path = tmpfile.name
+
+    if verbose:
+        stdout_setting = None
+        stderr_setting = None
+    else:
+        stdout_setting = subprocess.DEVNULL
+        stderr_setting = subprocess.DEVNULL
+
     subprocess.run(
         ["ffmpeg", "-i", input_path, "-y", output_path],
         check=True,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
+        stdout=stdout_setting,
+        stderr=stderr_setting,
         text=True,
     )
 
