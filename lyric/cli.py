85e404050441e483ad6acdc5da2fb7d981972af1
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-24 01:47:37 +0800

    fix: replace incorrect key with the correct key

--- lyric/cli.py
+++ lyric/cli.py
@@ -1,10 +1,11 @@
 import typer
-from scraper import fetch_lyrics, fetch_songs
+
+from lyric.scraper import fetch_lyrics, fetch_songs
 
 app = typer.Typer(help="Lyric related commands")
 
 
-@app.command(name="fetch-lyric")
+@app.command(name="fetch")
 def select_song(
     query: str,
     output: str = typer.Option(None, "--output", "-o", help="File to write results to"),
@@ -12,7 +13,7 @@ def select_song(
     results = fetch_songs(query)
 
     for i, result in enumerate(results, 1):
-        typer.echo(f"{i}. {result['name']}")
+        typer.echo(f"{i}. {result['title']}")
 
     choice = int(typer.prompt("Pick a number")) - 1
     if choice >= 0 and choice <= len(results):
@@ -24,7 +25,7 @@ def select_song(
                     f.write(lyrics)
                     typer.echo(f"Lyrics written to {output}")
                 else:
-                    typer.echo(f"Could not find lyrics for {results[choice]['name']}")
+                    typer.echo(f"Could not find lyrics for {results[choice]['title']}")
                     typer.Exit(code=1)
         else:
             print(lyrics)
