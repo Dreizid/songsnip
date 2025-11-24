e3f7700341c20ac5aa57a82d5489cb217578f5de
Andrei Adolfo <127655251+Dreizid@users.noreply.github.com>
2025-11-25 01:41:00 +0800

    fix: update the path to point exactly to the output directory instead of just returning the argument in output path

--- audio/splitter.py
+++ audio/splitter.py
@@ -1,11 +1,13 @@
 import atexit
+import re
 import tempfile
+from pathlib import Path
 from typing import Optional
 
 import demucs.separate
 
 
-def split_audio(audio_path: str, output_path: Optional[str] = None) -> str:
+def split_audio(audio_path: str, output_path: Optional[str] = None):
     if output_path is None:
         TMPDIR = tempfile.TemporaryDirectory()
         atexit.register(TMPDIR.cleanup)
@@ -13,4 +15,12 @@ def split_audio(audio_path: str, output_path: Optional[str] = None) -> str:
     args = ["--two-stems", "vocals", "-n", "htdemucs_ft", audio_path, "-o", output_path]
     demucs.separate.main(args)
 
-    return output_path
+    REGEX_PATTERN = r"([\w\s\-_.()]+\.wav)$"
+    audio_name_match = re.search(REGEX_PATTERN, audio_path)
+    if audio_name_match:
+        audio_name_str = audio_name_match.group(1)
+        audio_name = Path(audio_name_str).stem
+    else:
+        raise ValueError(f"Could not extract .wav filename from path: {audio_path}")
+
+    return str(Path(output_path) / "separated" / "htdemucs_ft" / audio_name)
